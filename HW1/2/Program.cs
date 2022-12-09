21eca8baf8cc0a3b563afb8645f4b2aa98a46ff1
Dima <dimdimonish@yandex.ru>
2022-12-09 15:16:40 +0400

    Problem 2 solved

---
+++ HW1/2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2, которое не равно числу 1: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Большее из двух чисел: ");
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else Console.WriteLine(number2);
