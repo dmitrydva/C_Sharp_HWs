8b22beca209a2a13be81c8d75f55c447b7b20043
Dima <dimdimonish@yandex.ru>
2022-12-09 16:24:48 +0400

    Problem 8 solved

---
+++ HW1/8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = 2;
+Console.Write("Чётные числа от 1 до N: ");
+while (N <= number)
+{
+    if(N==number || N==(number-1))
+    {
+        Console.Write(N);
+    }
+    
+    else Console.Write(N + ", ");
+    N += 2;
+}
