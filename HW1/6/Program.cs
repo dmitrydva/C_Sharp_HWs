d6b8a7eea2c0acef1f47b64be1e4c0636202c4d1
Dima <dimdimonish@yandex.ru>
2022-12-09 16:08:59 +0400

    Problem 6 solved

---
+++ HW1/6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( (Math.Abs(number) % 2) == 1 )
+{
+    Console.WriteLine("Число нечётное");
+}
+else Console.WriteLine("Число чётное");
