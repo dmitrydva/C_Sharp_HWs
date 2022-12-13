f4316e4393f8169cbf7a0afdf715027aa3687405
Dima <dimdimonish@yandex.ru>
2022-12-14 02:57:15 +0400

    10,13,15 solved

---
+++ HW2/13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    string str = number.ToString();
+    Console.Write("Третья цифра числа: ");
+    Console.WriteLine(str[2]);
+}
+else if (number < 0) Console.WriteLine("Число отрицательно");
+else if (number < 100) Console.WriteLine("Третьей цифры нет");
+
+
---
+++ HW2/15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число, обозначающее день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number < 6) Console.Write("День не является выходным");
+else if (number > 5 && number < 8) Console.WriteLine("День является выходным");
+else if (number > 7 || number < 1) Console.WriteLine("Число не обозначает день недели");
+
+
