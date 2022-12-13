eebce613a91e337849cd00a703e81503530747ac
Dima <dimdimonish@yandex.ru>
2022-12-14 02:21:16 +0400

    10 solved

---
+++ HW2/10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите положительное трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.Write("Вторая цифра числа: ");
+    int N = (number / 10) % 10;
+    Console.WriteLine(N);
+}
+else Console.WriteLine("Число не подходит -_- ");
+
