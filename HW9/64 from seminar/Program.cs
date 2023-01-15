d160b99598a897a8a66f8cacd2dd201b84b4c7f8
Dima <dimdimonish@yandex.ru>
2023-01-15 12:15:42 +0400

    HW9 solved v3

---
+++ HW9/64 from seminar/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите m (m<=n):");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n (m<=n):");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+string PrintNumbers(int start, int end)
+{
+    //if (start==0) return PrintNumbers(start + 1, end);
+    if (start == end) return start.ToString();
+    return (start + ", " + PrintNumbers(start + 1, end));
+}
+
+
+Console.WriteLine($"Все числа в промежутке от m до n: {PrintNumbers(m, n)}");
